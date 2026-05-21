836ed1ad9fe6bc8ff536612a203022c84675f127
tanyelrosell <107679280+marupoix@users.noreply.github.com>
2026-05-21 14:09:30 +0800

    Initial commit

---
+++ src/components/Projects.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="pb-5" id="projects">
+        <h1 class="mt-5 mb-5 pb-4 text-center">My Projects</h1>
+        <!-- Lets implement map method so that we can show all the porjects thru using the ProjectCard component -->
+        <div v-for="(group, index) in chunkedProjects" :key="index" class="card-deck my-5 justify-content-center">
+            <ProjectCard 
+                v-for="project in group"
+                :key="project.id"
+                :project="project"
+            />
+        </div>
+
+    </section>
+</template>
+
+<script setup>
+    import ProjectCard from "./ProjectCard.vue";
+    import { computed } from "vue"
+    import projects from "../data/projects.json"
+
+    const chunkSize = 3;
+
+    // this function is responsible for splitting the projects array into smaller arrays (chunks) with 3 chunkSize (elements) per array.
+    const chunkedProjects = computed(() => {
+        const chunks = [];
+        for (let i = 0; i < projects.length; i += chunkSize) {
+            chunks.push(projects.slice(i, i + chunkSize))
+        }
+        return chunks;
+    })
+
+
+</script>
